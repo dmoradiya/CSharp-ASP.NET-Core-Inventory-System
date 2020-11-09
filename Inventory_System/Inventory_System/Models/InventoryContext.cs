7ebf7fa6f8b3c29a860d0e5a1bb5a4a8515b659e
dhmec <dharmoradiya@gmail.com>
2020-11-09 14:16:14 -0700

    Create Product Model and InventoryContext

---
+++ Inventory_System/Inventory_System/Models/InventoryContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inventory_System.Models
+{
+    public class InventoryContext : DbContext
+    {
+        public virtual DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string connection =
+                    "server=localhost;" +
+                    "port=3306;" +
+                    "user=root;" +
+                    "database=mvc_inventory;";
+
+                string version = "10.4.14-MariaDB";
+
+                optionsBuilder.UseMySql(connection, x => x.ServerVersion(version));
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Name)
+                .HasCharSet("utf8mb4")
+                .HasCollation("utf8mb4_general_ci");
+
+            });
+        }
+    }
+}
