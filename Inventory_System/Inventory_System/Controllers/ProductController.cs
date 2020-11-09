ff2df80e0fd53c105198d1c9467125ce85faf168
dhmec <dharmoradiya@gmail.com>
2020-11-09 15:03:45 -0700

    Add ValidationException Class in Model

---
+++ Inventory_System/Inventory_System/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory_System.Controllers
+{
+    public class ProductController : Controller
+    {
+        // Action
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Method
+        public void CreateProduct(string id, string name, string quantity, string discontinue)
+        {
+            
+        }
+    }
+}
